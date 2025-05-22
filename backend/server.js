f1f9ed166781f75ecf3b23a5a6373f4c6bc10bc7
abexCOM <abdulakimustefa@gmail.com>
2025-05-22 15:32:24 +0300

    add

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const { connectDB } = require("./config/db");
 const userRoutes = require("./routes/UserRoutes"); // ✅ Renamed
 const adminRoutes = require("./routes/adminRoutes");
-
+const mongoose = require("mongoose");
 dotenv.config();
 
 const app = express();
@@ -24,8 +24,24 @@ connectDB();
 // app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes); //  Updated route path
 app.use("/api/admin", adminRoutes);
+app.get("/api/status", async (req, res) => {
+  try {
+    const state = mongoose.connection.readyState;
+    const states = ["Disconnected", "Connected", "Connecting", "Disconnecting"];
+
+    res.json({
+      message: "MongoDB status check",
+      status: states[state],
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error checking DB status", error: err.message });
+  }
+});
+
 app.all("*", (req, res) => {
-  res.status(404).json({ message: `Route  ${req.originalUrl} not found` });
+  res.status(404).json({ message: `Route  ${req.originalUrl} is  not found` });
 });
 // Start the server
 app.listen(PORT, () => {
--- frontend/src/baseUrl/baseUrl.jsx
+++ frontend/src/baseUrl/baseUrl.jsx
@@ -1,2 +1,2 @@
-const baseUrl = "https://abbabiyo-web-backend.abdiko.com";
+const baseUrl = "https://abbabiyo-web-backend.abdiko.com"; //"http://localhost:5000";
 export default baseUrl;
