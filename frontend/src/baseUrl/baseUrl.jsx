f8315ee141185aaefc3331102f96557c6bed31cc
abexCOM <abdulakimustefa@gmail.com>
2025-05-25 14:23:35 +0300

    remove unnessary files

--- frontend/src/baseUrl/baseUrl.jsx
+++ frontend/src/baseUrl/baseUrl.jsx
@@ -1,4 +1,3 @@
 const baseUrl = "https://abbabiyo-web-backend.abdiko.com"; //"http://localhost:5000";
-
 // const baseUrl = "https://abbabiyo-web-backend.abdiko.com"; // Production URL
 export default baseUrl;
